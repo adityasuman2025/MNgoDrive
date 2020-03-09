ae97fc67e1a9dc33fc7f9a57238714db70274609
adityasuman2025 <aditya.me16@iitp.ac.in>
2020-03-09 12:08:56 +0530

    custom context menu done in folder page

--- folder.php
+++ folder.php
@@ -83,19 +83,41 @@
 		<div class="overlay_content">dsf</div>
 	</div>
 
-<!--new file upload modal--------->
+<!------new file upload modal--------->
 	<div id="upload_file_sample">
 		<input type="file" name="file" id="file">
 		<br />
 		<div class="error"></div>
 	</div>
 
+<!------rename file/folder modal--------->
+	<div id="rename_file_folder_sample">
+		<input type="text" id="rename_file_folder_text_input" placeholder="New Name" />
+		<br />
+		<div class="button-5" id="rename_btn">
+		    <div class="translate"></div>
+		   	<button class="button_btn">Rename</button>
+		</div>
+		<div class="error"></div>
+	</div>
+
+<!------delete file/folder modal--------->
+	<div id="delete_file_folder_sample">
+		<div class="name_text">Are you sure to delete</div>
+		
+		<div class="button-5" id="yes_btn">
+		    <div class="translate"></div>
+		   	<button class="button_btn">Yes</button>
+		</div>
+		<div class="button-5" id="cancel_btn">
+		    <div class="translate"></div>
+		   	<button class="button_btn">Cancel</button>
+		</div>
+		<div class="error"></div>
+	</div>
+
 <!------custom context menu----------->
-	<ul class='custom-menu'>
-		<li data-action = "first">First thing</li>
-		<li data-action = "second">Second thing</li>
-		<li data-action = "third">Third thing</li>
-	</ul>
+	<ul class='custom-menu'></ul>
 
 <!-------script-------->
 	<script type="text/javascript">
@@ -105,52 +127,194 @@
 		logged_user_id = "<?php echo $logged_user_id; ?>";
 		folder_id = "<?php echo $folder_id; ?>";
 
+	//date and tym formatter
+		var dateFormat = function () 
+	    {
+		    var token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
+		        timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
+		        timezoneClip = /[^-+\dA-Z]/g,
+		        pad = function (val, len) {
+		            val = String(val);
+		            len = len || 2;
+		            while (val.length < len) val = "0" + val;
+		            return val;
+		        };
+
+		    // Regexes and supporting functions are cached through closure
+		    return function (date, mask, utc) {
+		        var dF = dateFormat;
+
+		        // You can't provide utc if you skip other args (use the "UTC:" mask prefix)
+		        if (arguments.length == 1 && Object.prototype.toString.call(date) == "[object String]" && !/\d/.test(date)) {
+		            mask = date;
+		            date = undefined;
+		        }
+
+		        // Passing date through Date applies Date.parse, if necessary
+		        date = date ? new Date(date) : new Date;
+		        if (isNaN(date)) throw SyntaxError("invalid date");
+
+		        mask = String(dF.masks[mask] || mask || dF.masks["default"]);
+
+		        // Allow setting the utc argument via the mask
+		        if (mask.slice(0, 4) == "UTC:") {
+		            mask = mask.slice(4);
+		            utc = true;
+		        }
+
+		        var _ = utc ? "getUTC" : "get",
+		            d = date[_ + "Date"](),
+		            D = date[_ + "Day"](),
+		            m = date[_ + "Month"](),
+		            y = date[_ + "FullYear"](),
+		            H = date[_ + "Hours"](),
+		            M = date[_ + "Minutes"](),
+		            s = date[_ + "Seconds"](),
+		            L = date[_ + "Milliseconds"](),
+		            o = utc ? 0 : date.getTimezoneOffset(),
+		            flags = {
+		                d:    d,
+		                dd:   pad(d),
+		                ddd:  dF.i18n.dayNames[D],
+		                dddd: dF.i18n.dayNames[D + 7],
+		                m:    m + 1,
+		                mm:   pad(m + 1),
+		                mmm:  dF.i18n.monthNames[m],
+		                mmmm: dF.i18n.monthNames[m + 12],
+		                yy:   String(y).slice(2),
+		                yyyy: y,
+		                h:    H % 12 || 12,
+		                hh:   pad(H % 12 || 12),
+		                H:    H,
+		                HH:   pad(H),
+		                M:    M,
+		                MM:   pad(M),
+		                s:    s,
+		                ss:   pad(s),
+		                l:    pad(L, 3),
+		                L:    pad(L > 99 ? Math.round(L / 10) : L),
+		                t:    H < 12 ? "a"  : "p",
+		                tt:   H < 12 ? "am" : "pm",
+		                T:    H < 12 ? "A"  : "P",
+		                TT:   H < 12 ? "AM" : "PM",
+		                Z:    utc ? "UTC" : (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
+		                o:    (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4),
+		                S:    ["th", "st", "nd", "rd"][d % 10 > 3 ? 0 : (d % 100 - d % 10 != 10) * d % 10]
+		            };
+
+		        return mask.replace(token, function ($0) {
+		            return $0 in flags ? flags[$0] : $0.slice(1, $0.length - 1);
+		        });
+		    };
+		}();
+
+			// Some common format strings
+				dateFormat.masks = {
+				    "default":      "ddd mmm dd yyyy HH:MM:ss",
+				    shortDate:      "m/d/yy",
+				    mediumDate:     "mmm d, yyyy",
+				    longDate:       "mmmm d, yyyy",
+				    fullDate:       "dddd, mmmm d, yyyy",
+				    shortTime:      "h:MM TT",
+				    mediumTime:     "h:MM:ss TT",
+				    longTime:       "h:MM:ss TT Z",
+				    isoDate:        "yyyy-mm-dd",
+				    isoTime:        "HH:MM:ss",
+				    isoDateTime:    "yyyy-mm-dd'T'HH:MM:ss",
+				    isoUtcDateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'"
+				};
+
+			// Internationalization strings
+				dateFormat.i18n = {
+				    dayNames: [
+				        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
+				        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+				    ],
+				    monthNames: [
+				        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+				        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+				    ]
+				};
+
+			// For convenience...
+			Date.prototype.format = function (mask, utc) {
+			    return dateFormat(this, mask, utc);
+		};
+
 	//function to show custom context menu
 		document.addEventListener('contextmenu', event => event.preventDefault()); //removing default context menu
 
 		function showCustomContext(folder_class)
 		{
+			var html = "<li>Open</li><li>Rename</li><li>Delete</li><li>Details</li>";
+
 		// Trigger action when the contexmenu is about to be shown
-			$('.' + folder_class).bind("contextmenu", function (event) {
-			    
-			    // Avoid the real one
+			$('.' + folder_class).bind("contextmenu", function (event)
+			{
+			//getting the details of the selected folder/file	
+				var type = $(this).attr('type');
+				var name_text = $(this).find('.name_text').text().trim();
+				if(type == "folder")
+				{
+					var folder_id =  $(this).attr('folder_id');
+
+					$(".custom-menu").html(html);
+					$(".custom-menu").attr('folder_id', folder_id);
+					$(".custom-menu").attr('name_text', name_text);
+					$(".custom-menu").attr('file_address', "folder_address_not_allowed");
+				}
+				else if(type == "file")
+				{
+					var file_id =  $(this).attr('file_id');
+					var file_address = $(this).attr('file_address');
+
+					$(".custom-menu").html(html);
+					$(".custom-menu").append("<li>Share</li>");
+					$(".custom-menu").attr('file_id', file_id);
+					$(".custom-menu").attr('file_address', file_address);
+					$(".custom-menu").attr('name_text', name_text);					
+				}
+				$(".custom-menu").attr('type', type);
+
+		    // Avoid the real one
 			    event.preventDefault();
 			    
-			    // Show contextmenu
+		    // Show contextmenu
 			    $(".custom-menu").finish().toggle(100).
 			    
-			    // In the right position (the mouse)
+		    // In the right position (the mouse)
 			    css({
 			        top: event.pageY + "px",
 			        left: event.pageX + "px"
 			    });
+
+			// If the menu element is clicked
+				$(".custom-menu li").click(function()
+				{
+			    // This is the triggered action name
+			    	var text = $(this).text().trim();
+				    switch(text) 
+				    {
+				        case "Open": open_File_Folder($(this).parent()); break;
+				        case "Rename": rename_File_Folder($(this).parent()); break;
+				        case "Delete": delete_File_Folder($(this).parent()); break;
+				        case "Share": share_File($(this).parent()); break;
+				        case "Details": details_File_Folder($(this).parent()); break;
+				    }
+				  
+			    // Hide it AFTER the action was triggered
+				    $(".custom-menu").hide(100);
+				});
 			});
 
 		// If the document is clicked somewhere
-			$(document).bind("mousedown", function (e) {
-			    
+			$(document).bind("mousedown", function (e) 
+			{
 			    // If the clicked element is not the menu
 			    if (!$(e.target).parents(".custom-menu").length > 0) {
-			        
 			        // Hide it
 			        $(".custom-menu").hide(100);
 			    }
-			});
-
-		// If the menu element is clicked
-			$(".custom-menu li").click(function(){
-			    
-			    // This is the triggered action name
-			    switch($(this).attr("data-action")) {
-			        
-			        // A case for each action. Your actions here
-			        case "first": alert("first"); break;
-			        case "second": alert("second"); break;
-			        case "third": alert("third"); break;
-			    }
-			  
-			    // Hide it AFTER the action was triggered
-			    $(".custom-menu").hide(100);
 			});
 		}
 
@@ -161,6 +325,13 @@
 			{
 				location.href = "index.php";
 			});
+		});
+
+	//on clicking on close btn
+		$('.close_overlay_btn, .overlay_backgrnd').on("click", function()
+		{
+			$('.overlay_backgrnd').fadeOut(200);
+			$('.overlay_div').fadeOut(200);
 		});
 
 	//on clicking on upload file btn
@@ -263,6 +434,7 @@
 					{
 						var icon_name = "file";
 						var file_address = resultArray[index]['file_address'];
+						var file_id = resultArray[index]['file_id'];
 
 						var name_extension = name.split('.').pop().toLowerCase();
 						var only_name = name.split('.').slice(0, -1).join('.')
@@ -270,7 +442,7 @@
 
 						var display_name = only_name + "." + name_extension;
 
-						tempHTML += '<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 x_m-p file_folder_container" type="' + type + '" file_address="' + file_address + '"><img src="img/' + icon_name + '.png" /><div class="name_text">' + display_name + '</div></div>';
+						tempHTML += '<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 x_m-p file_folder_container" type="' + type + '" file_id="' + file_id + '" file_address="' + file_address + '"><img src="img/' + icon_name + '.png" /><div class="name_text">' + display_name + '</div></div>';
 					}
 
 					html += tempHTML;
@@ -300,7 +472,7 @@
 		        {
 		        }, "POST", "_blank");
 			}
-			else //displaying the content of the folder
+			else if(type == "folder") //displaying the content of the folder
 			{
 				var folder_id = $(_this_).attr("folder_id");		
 				$.redirect("folder.php",
@@ -308,6 +480,218 @@
 		        	folder_id: folder_id
 		        }, "POST");
 			}
+		}	
+	
+	//function to rename folder/file
+		function rename_File_Folder(_this_)
+		{
+		//displaying the overlay div and its content	
+			$('.overlay_backgrnd').fadeIn(400);
+			$('.overlay_div').fadeIn(400);
+
+			var old_name = $(_this_).attr('name_text');			
+			// $('#rename_file_folder_sample #rename_file_folder_text_input').attr("value", old_name);
+
+			var html = $('#rename_file_folder_sample').html();
+			$('.overlay_content').html(html);
+
+		//on pressing rename btn
+			$('#rename_btn').on("click", function()
+			{
+				$('.error').text("");
+				$('.error').html("<img class=\"gif_loader\" src=\"img/loader1.gif\">");
+
+				var new_name = $('#rename_file_folder_text_input').val().trim();
+				
+				var type = $(_this_).attr("type");				
+				if(type == "file")
+					var id = $(_this_).attr("file_id");
+				else if(type == "folder")
+					var id = $(_this_).attr("folder_id");				
+				
+			//sending rqst to api	
+				var post_address = api_address + "rename_file_folder.php";
+				$.post(post_address, {logged_user_id: logged_user_id, type: type, id: id, old_name: old_name, new_name: new_name}, function(data)
+				{
+					// console.log(data);
+
+					if(data == -100)
+					{
+						$('.error').text("Database connection error");
+					}
+					else if(data == -1)
+					{
+						$('.error').text("Something went wrong");
+					}
+					else if(data == 0)
+					{
+						$('.error').text("Fail to rename");
+					}
+					else if(data == 1) //renamed successfully
+					{						
+					 	location.reload();
+					}
+					else
+					{
+						$('.error').text("Unknown error");
+					}
+				});
+			});
+		}
+
+	//function to delete folder/file
+		function delete_File_Folder(_this_)
+		{
+		//displaying the overlay div and its content	
+			$('.overlay_backgrnd').fadeIn(400);
+			$('.overlay_div').fadeIn(400);
+
+			var html = $('#delete_file_folder_sample').html();
+			$('.overlay_content').html(html);
+
+		//on pressing cancel btn
+			$('#cancel_btn').on("click", function()
+			{
+				$('.overlay_backgrnd').fadeOut(200);
+				$('.overlay_div').fadeOut(200);
+			});
+
+		//on pressing yes btn
+			$('#yes_btn').on("click", function()
+			{
+				$('.error').text("");
+				$('.error').html("<img class=\"gif_loader\" src=\"img/loader1.gif\">");
+
+				var type = $(_this_).attr("type");				
+				if(type == "file")
+					var id = $(_this_).attr("file_id");
+				else if(type == "folder")
+					var id = $(_this_).attr("folder_id");				
+				
+			//sending rqst to api	
+				var post_address = api_address + "delete_file_folder.php";
+				$.post(post_address, {logged_user_id: logged_user_id, type: type, id: id}, function(data)
+				{
+					// console.log(data);
+
+					if(data == -100)
+					{
+						$('.error').text("Database connection error");
+					}
+					else if(data == -1)
+					{
+						$('.error').text("Something went wrong");
+					}
+					else if(data == 0)
+					{
+						$('.error').text("Fail to rename");
+					}
+					else if(data == 1) //renamed successfully
+					{						
+					 	location.reload();
+					}
+					else
+					{
+						$('.error').text("Unknown error");
+					}
+				});
+			});
+		}
+	
+	//function to share file location/link
+		function share_File(element)
+		{
+			var web_address   = window.location.origin;   // Returns base URL (https://example.com)
+			if(web_address == "http://localhost") //for local server
+				web_address = "http://localhost/MNgoDrive";
+			
+			var file_address = $(element).attr("file_address");
+			var full_address = web_address + "/" + file_address;
+
+		//copy to clipboard stuffs	
+			var $temp = $("<input>");
+			$("body").append($temp);
+			$temp.val(full_address).select();
+			document.execCommand("copy");
+			$temp.remove();
+
+		//displaying the overlay div and its content	
+			$('.overlay_backgrnd').fadeIn(400);
+			$('.overlay_div').fadeIn(400);
+					
+			$('.overlay_content').html('<div style="color: #f1f1f1; font-size: 150%;">copied</div>');
+		}
+	
+	//function to get details of file/folder
+		function details_File_Folder(_this_)
+		{
+		//displaying the overlay div and its content	
+			$('.overlay_backgrnd').fadeIn(400);
+			$('.overlay_div').fadeIn(400);
+					
+			$('.overlay_content').html("<img class=\"gif_loader\" src=\"img/loader1.gif\">");
+
+			var type = $(_this_).attr("type");				
+			if(type == "file")
+				var id = $(_this_).attr("file_id");
+			else if(type == "folder")
+				var id = $(_this_).attr("folder_id");				
+			
+		//sending rqst to api	
+			var post_address = api_address + "get_details_of_file_folder.php";
+			$.post(post_address, {logged_user_id: logged_user_id, type: type, id: id}, function(data)
+			{
+				// console.log(data);
+
+				if(data == -100)
+				{
+					$('.error').text("Database connection error");
+				}
+				else if(data == -1)
+				{
+					$('.error').text("Something went wrong");
+				}
+				else
+				{
+					var html = "";
+
+					var resultArray = $.parseJSON(data);				
+					for(var index in resultArray) 
+					{
+						var tempHTML = "";
+
+						var name = (resultArray[index]['name']);
+						var type = resultArray[index]['type'];
+
+						if(type == "folder")
+						{							
+							var icon_name = "folder";
+
+							var added_on = resultArray[index]['added_on'];
+
+							tempHTML += '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 x_m-p details_container"><div class="row"><div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 x_m-p details_img_cont"><img src="img/' + icon_name + '.png" /></div><div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 x_m-p details_details_cont"> <b> Name: </b> ' + name + '<br /> <b> Added on: </b> ' + dateFormat(added_on, "dd mmmm yy - h:MM TT") + '</div></div></div>';
+						}
+						else if(type == "file")
+						{
+							var icon_name = "file";
+
+							var added_on = resultArray[index]['added_on'];
+							var file_address = resultArray[index]['file_address'];
+
+							var web_address   = window.location.origin;   // Returns base URL (https://example.com)
+							if(web_address == "http://localhost") //for local server
+								web_address = "http://localhost/MNgoDrive";
+							
+							var full_address = web_address + "/" + file_address;
+
+							tempHTML += '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 x_m-p details_container"><div class="row"><div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 x_m-p details_img_cont"><img src="img/' + icon_name + '.png" /></div><div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 x_m-p details_details_cont"> <b> Name: </b> ' + name + '<br /> <b> Added on: </b> ' + dateFormat(added_on, "dd mmmm yy - h:MM TT") + '<br /> <b> File Address: </b> <a target="_blank" href="' + full_address + '">' + full_address + '</a></div></div></div>';
+						}
+
+						html += tempHTML;
+					}
+					$('.overlay_content').html(html);
+				}
+			});
 		}
 	</script>
 </body>
